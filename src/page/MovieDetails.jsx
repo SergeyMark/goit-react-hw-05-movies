d095031d58da755be8786acb8425706d04bd1fad
Sergey <110375004+SergeyMark@users.noreply.github.com>
2023-03-31 14:56:23 +0300

    Update MovieDetails.jsx

--- src/page/MovieDetails.jsx
+++ src/page/MovieDetails.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import { FeatchMovie } from "components/FetchMovie";
+
+import { useState, useEffect } from "react";
+import { Outlet, Link } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
+import { useRef} from "react";
 
 export const MovieDetails = () => {
-    return <div>MovieDetails</div>
+    
 }
